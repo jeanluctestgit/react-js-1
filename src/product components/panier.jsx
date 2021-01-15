3e75f98a2b6d2639c54a06e79eb725d4a17ae623
unknown <jeanluc.deshayes92@gmail.com>
2021-01-15 11:06:34 +0100

    maj num item

--- src/product components/panier.jsx
+++ src/product components/panier.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Panier(props) {
     return (
         <div>
-            <p>Number Item : { props.numItem }</p>
+            <p>Number Item : <span className = "badge badge-primary">{ props.numItem }</span></p>
             <p className = "badge badge-primary w-100" ><span className="float-left">Total :</span> <span className="float-right">{ props.total }</span></p>
         </div>
     )
